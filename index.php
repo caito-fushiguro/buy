412eef56796dfe5e0330c28d2c6410a133ba984e
caito-fushiguro <141917232+caito-fushiguro@users.noreply.github.com>
2023-10-15 15:16:42 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My Website</title>
+</head>
+<body>
+  <h1>Welcome to My Website</h1>
+  <p>This is a simple website built using HTML.</p>
+  <img src="image.jpg" alt="Example Image">
+  <ul>
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+  </ul>
+  <button>Click Me</button>
+</body>
+</html>
